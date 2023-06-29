7d3510d2cc9d47be3f485c67af3a7af1293f3b45
Marín Alcaraz <alcara_m@pm.me>
2023-06-28 22:10:35 -0700

    Add status manipulation, refactor interface to represent status properly and update compiler config

--- src/6502.ts
+++ src/6502.ts
@@ -1,3 +1,8 @@
+// 6502 is a little endian system.
+// Little-endian is a byte-ordering architecture used in computer systems, including the 6502 processor.
+// In a little-endian architecture, the least significant byte of a multi-byte value is stored at the
+// lowest memory address, while the most significant byte is stored at the highest memory address.
+// This means that the bytes you write can be read from left to right.
 //
 // Instruction Set: The 6502 has a relatively small instruction set compared to modern CPUs.
 // Some common instruction types are:
@@ -10,62 +15,59 @@
 // Comparison
 // Branching
 
-const MAX_MEMORY = 1024 * 64
-
-type Byte = string
-type Word = Number
+type Byte = number // 8 bits
+type Word = number // 16 bits
 type Register = Byte // 8 bits
-type Flag = Byte
 
 // The NES has a specific memory map, which designates different regions of memory for various purposes.
 // For example, certain memory locations store the program code, while others hold data or act as memory-mapped I/O
 // registers for interacting with hardware components.
-
 type Memory = Byte[]
 
 type SixFiveZeroTwo = {
   programCounter: Word // 16 bits
   stackPointer: Byte
-  status: Byte
 
   accumulator: Register
   registerX: Register
   registerY: Register
 
-  carry: Flag
-  zero: Flag
-  decimalMode?: Flag // Not used by NES
-  breakCommand: Flag
+  // Status represents the following flags. Flags denote a status and they can be on/off
+  // 7 6 5 4 3 2 1 0
+  // N V _ B D I Z C
 
-  // Reserved
-  interruptDisable: Flag // FFFA - FFFB
-  overflow: Flag // FFFC - FFFD
-  negative: Flag // FFFE - FFFF
+  // N - Negative
+  // V - Overflow
+  // _ - Unused (always 1)
+  // B - Break command
+  // D - Decimal mode
+  // I - Interrupt disable
+  // Z - Zero
+  // C - Carry
 
-  mem: Memory
+  status: Byte
+
+  memory: Memory // 64KB
 }
 
-// 6502 is a little endian system.
-// Little-endian is a byte-ordering architecture used in computer systems, including the 6502 processor.
-// In a little-endian architecture, the least significant byte of a multi-byte value is stored at the
-// lowest memory address, while the most significant byte is stored at the highest memory address.
-// This means that the bytes you write can be read from left to right.
+// Flags bits
+const CARRY_FLAG = 0; // Carry
+const ZERO_FLAG = 1; // Zero
+const INTERRUPT_DISABLE_FLAG = 2; // Interrupt Disable
+const DECIMAL_MODE_FLAG = 3; // Decimal Mode (Not used in NES)
+const BREAK_COMMAND_FLAG = 4; // Break Command
+const UNUSED_FLAG = 5; // Unused
+const OVERFLOW_FLAG = 6; // Overflow
+const NEGATIVE_FLAG = 7; // Negative
 
-let chip: SixFiveZeroTwo = {
-  programCounter: 0x0000,
-  stackPointer: '',
-  status: '',
-  accumulator: '',
-  registerX: '',
-  registerY: '',
-  carry: '',
-  zero: '',
-  decimalMode: '',
-  breakCommand: '',
-  interruptDisable: '',
-  overflow: '',
-  negative: '',
-  mem: []
+function setFlag(cpu: SixFiveZeroTwo, bit: number, value: boolean) {
+    if (value) {
+        cpu.status |= (1 << bit);
+    } else {
+        cpu.status &= ~(1 << bit);
+    }
 }
 
-console.log("A CPU in isolation", chip)
+function isFlagSet(cpu: SixFiveZeroTwo, bit: number): boolean {
+    return (cpu.status & (1 << bit)) !== 0;
+}
